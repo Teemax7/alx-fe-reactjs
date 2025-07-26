1b8b10ae88400c60a0da4e5b35d4396b480b67f7
Akinloye Timilehin <oladiranakinloye@yahoo.com>
2025-07-26 20:46:32 +0100

    Task 0: Apply inline CSS styling to components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
