7eec3abecf9d5f3ecafe25688b6b3f8158be4260
Akinloye Timilehin <oladiranakinloye@yahoo.com>
2025-08-26 00:23:46 +0100

    add useAuth hook and integrate in ProtectedRoute

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
 import { Navigate } from "react-router-dom";
-
-// Simulated authentication status
-const isAuthenticated = false; // toggle to true for testing
+import useAuth from "../hooks/useAuth.jsx";
 
 const ProtectedRoute = ({ children }) => {
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  const { user } = useAuth(); // use the hook
+
+  return user ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
