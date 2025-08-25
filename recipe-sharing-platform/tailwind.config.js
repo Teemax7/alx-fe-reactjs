dc0018bc95911aea51a65c9472aa54b6bbfa81c2
Akinloye Timilehin <oladiranakinloye@yahoo.com>
2025-08-25 04:43:28 +0100

    fixed tailwind configuration

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,13 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: {} },
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",  // all JS/JSX/TS/TSX files in src
+    "./public/index.html"           // optional if you use public HTML
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
