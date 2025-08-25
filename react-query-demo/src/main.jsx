58bdfc3fda24d783b7ad09de67ca1ab0c9ee7c38
Akinloye Timilehin <oladiranakinloye@yahoo.com>
2025-08-25 23:57:20 +0100

    move QueryClientProvider setup to main.jsx

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -4,8 +4,10 @@ import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 
+// Create QueryClient instance
 const queryClient = new QueryClient();
 
+// Wrap your App with QueryClientProvider
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
